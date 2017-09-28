41118806b248755260d93373f6efa0d149a38651
MiningMarsh <miningmarsh@miningmarshs-imac.local>
2017-09-28 14:52:49 -0700

    Updated blood pressure measurement list to be sorted.

---
+++ BPApp/Shared/Config.swift
@@ -0,0 +1,60 @@
+//
+//  Config.swift
+//  BPApp
+//
+//  Created by MiningMarsh on 9/26/17.
+//  Copyright © 2017 BlackstoneBuilds. All rights reserved.
+//
+
+import Foundation
+import os.log
+
+public class Config {
+    
+    // Where to save and load patient data.
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let PatientStore = DocumentsDirectory.appendingPathComponent("patients")
+    
+    // The list of patients we save.
+    static public var patients: [Patient] {
+        get {
+            
+            // Lock object so that we don't get while someone is setting.
+            objc_sync_enter(self)
+            defer { objc_sync_exit(self) }
+            
+            if let restored = NSKeyedUnarchiver.unarchiveObject(withFile: Config.PatientStore.path) as? [Patient] {
+                
+                os_log("Patients restored.", log: OSLog.default, type: .debug)
+                return restored
+                
+            } else {
+                os_log("Failed to restore patients.", log: OSLog.default, type: .debug)
+                return []
+            }
+        }
+        
+        set(newPatients) {
+            
+            // Lock object so that we don't corrupt another lock operation.
+            objc_sync_enter(self)
+            defer { objc_sync_exit(self) }
+            
+            let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(
+                newPatients,
+                toFile: Config.PatientStore.path
+            )
+            
+            if isSuccessfulSave {
+                
+                os_log("Patients persisted.", log: OSLog.default, type: .debug)
+            } else {
+                
+                os_log("Failed to persist patients...", log: OSLog.default, type: .error)
+            }
+        }
+    }
+    
+    
+}
+
