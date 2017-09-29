d099e28b6775dc1e2e43c5813fae77d5a5fedbde
tlight6 <jakestar0306@comcast.net>
2017-09-28 20:09:47 -0400

    Added Settings screen and saveable measurement option
    The measurement option defaults depending on the system's region. It is not yet functional beyond saving.

--- BPApp/BPProfessional/BPProMainViewController.swift
+++ BPApp/BPProfessional/BPProMainViewController.swift
@@ -13,6 +13,18 @@ class BPProMainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // This sets the system's default measurement system as the app's default
+        if UserDefaults.standard.value(forKey: "numSystemChanged") as? Bool != true {
+            UserDefaults.standard.set(false, forKey: "numSystemChanged")
+            let isMetric = (Locale.current as NSLocale).object(forKey: NSLocale.Key.usesMetricSystem) as! Bool
+            if isMetric {
+                UserDefaults.standard.set(1, forKey: "numSystem")
+            } else {
+                UserDefaults.standard.set(0, forKey: "numSystem")
+            }
+        }
+        //print("numSystemChanged: ",UserDefaults.standard.value(forKey: "numSystemChanged") ?? "numSystemChanged not set")
+        //print("start numSystem: ",UserDefaults.standard.value(forKey: "numSystem") ?? "numSystem not set")
     }
 
     override func didReceiveMemoryWarning() {
