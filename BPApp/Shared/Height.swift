3fde3e13d541ea805ee16e1566a22bc199b9b9a6
MiningMarsh <miningmarsh@miningmarshs-imac.local>
2017-11-16 16:03:28 -0800

    Comments and code cleanup of height object.

--- BPApp/Shared/Height.swift
+++ BPApp/Shared/Height.swift
@@ -8,10 +8,13 @@
 
 import Foundation
 
+// Represents a height and auto converts between feet and meters.
 public class Height {
     
+    // Actual storage of height.
     public var meters = 0.0
     
+    // Convert the internal meters to feet.
     public var feet: Double {
         get {
             return self.meters * 3.28084
@@ -22,22 +25,32 @@ public class Height {
         }
     }
     
+    // Create a height object using meters.
     public init(heightInMeters: Double) {
         self.meters = heightInMeters
     }
     
+    // Create a height object using feet.
     public init(heightInFeet: Double) {
         self.feet = heightInFeet
     }
     
+    // Display the height in either meters or feet/in depending on the mode of the config.
     public var description: String {
         get {
+            
+            // Display in metric.
             if Config.unitSystem == Config.UnitSystem.metric {
                 return "\(self.meters) m"
+            
+            // Displayt in ft, in
             } else {
-                let conv = self.meters * 3.28084
+                
+                // Get the feet and inches part.
+                let conv = self.feet
                 let ft = floor(conv)
                 let inch = floor(conv.truncatingRemainder(dividingBy: 1.0) * 12)
+                
                 return "\(Int(ft)) ft \(Int(inch)) in"
             }
         }
