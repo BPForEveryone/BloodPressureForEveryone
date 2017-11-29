10a2c25f8a5ad1996590fd8a7b1b9e9a5aab1fb9
MiningMarsh <miningmarsh@miningmarshs-imac.local>
2017-11-28 22:03:31 -0800

    Blood Pressure for Everyone's settings screen has been revamped.

--- BPApp/BPEveryone/BPESettingsViewController.swift
+++ BPApp/BPEveryone/BPESettingsViewController.swift
@@ -8,31 +8,36 @@
 
 import UIKit
 
-class BPESettingsViewController : UIViewController {
+class BPESettingsViewController : UITableViewController {
+    
+    // The number system selector.
+    @IBOutlet weak var numSysSelection: UISegmentedControl!
+    
+    // Constructor.
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    // When going back, dismiss.
     @IBAction func back() {
         dismiss(animated: true, completion: nil)
     }
     
-    @IBOutlet weak var numSysSelection: UISegmentedControl!
-    
-    
-    @IBAction func save(_ sender: UIBarButtonItem) {
-        Config.unitSystem = numSysSelection.selectedSegmentIndex == 0 ? Config.UnitSystem.imperial : Config.UnitSystem.metric
-    }
-    
-    @IBAction func save2(_ sender: UIButton) {
-        Config.unitSystem = numSysSelection.selectedSegmentIndex == 0 ? Config.UnitSystem.imperial : Config.UnitSystem.metric
+    // Called when the user changes the number system selected.
+    @IBAction func numSysSelectionChanged(_ sender: Any) {
+        
+        // Change the saved unit system selection to store.
+        Config.unitSystem = numSysSelection.selectedSegmentIndex == 0 ? Config.UnitSystem.metric : Config.UnitSystem.imperial
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        
+        // Setup the segment control to match the stored system.
         if Config.unitSystem == Config.UnitSystem.imperial {
-            numSysSelection.selectedSegmentIndex = 0
-        } else {
             numSysSelection.selectedSegmentIndex = 1
+        } else {
+            numSysSelection.selectedSegmentIndex = 0
         }
     }
 }
